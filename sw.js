48edfad3eb6e36e8aedd2757cd7de5300e1f109d
TI30X <falketimo38@gmail.com>
2026-07-05 19:15:28 +0200

    UI: Schatten entfernt + Termin-HTML ganz öffnen (SW v71)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* TVZA Service Worker v70 - offline-first */
+/* TVZA Service Worker v71 - offline-first */
 
-const CACHE = 'tvza-v70';
+const CACHE = 'tvza-v71';
 const FIREBASE_SDK = [
   'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js',
   'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js',
